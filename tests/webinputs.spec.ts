d5d58cfb2790fad47fd62a6095f472b82f067a0d
Hubert Jangas <hubert.jangas@outlook.com>
2026-02-22 15:41:16 +0100

    Cookie button issues.

--- tests/webinputs.spec.ts
+++ tests/webinputs.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 // check whether page has title or not
 test('has title', async ({ page }) => {
   await page.goto('https://practice.expandtesting.com');
-  await page.getByRole('button', { name: 'Consent' }).click();
+  // await page.getByRole('button', { name: 'Consent' }).click();
   await expect(page).toHaveTitle(/Automation Testing Practice Website for QA and Developers | UI and API/);
 });
 
